14ce06582b5bca6d13930f2c957dbea63d96ccb9
FAHADLEE <fahadlee2000727@gmail.com>
2023-06-20 17:05:26 +0530

    Update For all page loctions

--- jessy-cabs/src/component/Dashboard/Maindashboard/MainDashboard.jsx
+++ jessy-cabs/src/component/Dashboard/Maindashboard/MainDashboard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./dashboard.css";
-import Sidebar from "../Sildebar/Slidebar";
 import { Outlet } from "react-router-dom";
+import Sidebar from "../MainDash/Sildebar/Slidebar";
 
 const MainDashboard = () => {
   return (
     <>
       <section className="dash-board">
         <div className="glass">
-          <Sidebar />
+          <Sidebar/>
           <Outlet />
         </div>
       </section>

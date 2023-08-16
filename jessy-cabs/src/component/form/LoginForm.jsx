a0859193f8879aebc0f4f56d4e03914d81573878
akash02899 <akash02899@gmail.com>
2023-08-16 11:17:03 +0530

    login page updated

--- jessy-cabs/src/component/form/LoginForm.jsx
+++ jessy-cabs/src/component/form/LoginForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import { emailValidator, passwordValidator } from "./regexValidator";
+import { emailValidator, passwordValidator } from "./regexValidator";
 import "./Form.css";
 import portalimg from "../../assets/img/portal-img.jpg";
 import { useNavigate } from "react-router-dom";
@@ -8,72 +8,55 @@ import { RiFacebookCircleFill } from "@react-icons/all-files/ri/RiFacebookCircle
 import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin";
 import { BiHide } from "@react-icons/all-files/bi/BiHide";
 import { AiOutlineEye } from "@react-icons/all-files/ai/AiOutlineEye";
-
 const Login = () => {
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
   const toggle = () => {
     setOpen(!open);
   };
-  // const [passwordVisible, setPasswordVisible] = useState(false); // Rename 'open' to 'passwordVisible'
-  const [input, setInput] = useState({ username: '', userpassword: '' });
-  const [errorMessage, setErrorMessage] = useState('');
-  const [successMessage, setSuccessMessage] = useState("");
-  // const [username] = useState("");
-  // const [userpassword] = useState("");
-
+  const [input, setInput] = React.useState({ username: "", password: "" });
+  const [errorMessage, seterrorMessage] = useState("");
+  const [successMessage, setsuccessMessage] = useState("");
   const handleChange = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
   };
-  
-  const formSubmitter = async (e) => {
-    e.preventDefault();
-    setErrorMessage('');
-  
-    try {
-      const response = await fetch('http://localhost:8081/usercreation', {
-        // method: 'POST',
-        // headers: {
-        //   'Content-Type': 'application/json',
-        // },
-        // body: JSON.stringify({
-        //   username: input.username, // Use input.username from state
-        //   userpassword: input.userpassword, // Use input.userpassword from state
-        // }),
-      });
-  
-      const data = await response.json();
-  
-      if (response.ok) {
-        // Login successful
-        setSuccessMessage(data.message);
-        localStorage.setItem('auth', true);
-        navigate('/home/dashboard');
-      } else {
-        // Login failed
-        setErrorMessage(data.error);
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      setErrorMessage('An error occurred');
-    }
-  };
 
+  React.useEffect(() => {
+    if (localStorage.getItem("auth")) navigate("/");
+  });
+
+  const formSumitter = (e) => {
+    e.preventDefault();
+    setsuccessMessage("");
+    if (!emailValidator(input.username))
+      return seterrorMessage("Please enter valid user id");
+
+    if (!passwordValidator(input.password))
+      return seterrorMessage(
+        "Password should have minimum 8 character with the combination of uppercase, lowercase, numbers and specialcharaters"
+      );
+    // setsuccessMessage('Successfully Validated');
+    if (input.username !== "admin@gmail.com" || input.password !== "Admin@321")
+      return seterrorMessage("Invalid user id");
+
+    navigate("/home/dashboard");
+    localStorage.setItem("auth", true);
+  };
   return (
     <div className="portal-container">
       <div className="glasses">
         <div className="left-col">
-          <img className="portalimg" src={portalimg} alt="portalimg" />
+          <img className="portalimg" src={portalimg} alt="portalimg"></img>
         </div>
         <div className="right-col">
-          <form className="portal" onSubmit={formSubmitter}>
-            <div className="title">Login</div>
-            {errorMessage && (
+          <form className="portal" onSubmit={formSumitter}>
+            <div className="title">login</div>
+            {errorMessage.length > 0 && (
               <div style={{ marginBottom: "10px", color: "red" }}>
                 {errorMessage}
               </div>
             )}
-            {successMessage && (
+            {successMessage.length > 0 && (
               <div style={{ marginBottom: "10px", color: "green" }}>
                 {successMessage}
               </div>
@@ -86,29 +69,30 @@ const Login = () => {
                 onChange={handleChange}
                 required
               />
-              <div className="under-line" />
+              <div className="under-line" required></div>
+
               <label>Username</label>
             </div>
             <div className="user-input">
               <input
-                type={open ? "text" : "password"}
+                type={open === false ? "password" : "text"}
                 name="password"
                 autoComplete="off"
                 onChange={handleChange}
                 required
               />
-              <div className="under-line" />
+              <div className="under-line" required></div>
               <label>Password</label>
               <div className="pass-hide">
-                {open ? (
-                  <AiOutlineEye onClick={toggle} />
-                ) : (
+                {open === false ? (
                   <BiHide onClick={toggle} />
+                ) : (
+                  <AiOutlineEye onClick={toggle} />
                 )}
               </div>
             </div>
             <div className="forget-link">
-              <a href="/">Forgot password</a>
+              <a href="/">forget password </a>
             </div>
             <div className="group button-group">
               <button type="submit" className="signup-btn">

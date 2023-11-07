916cc4282aaf77cdb48f5aef10fc29eacfba193f
Abdul Fahad <fahadlee2000727@gmail.com>
2023-11-07 11:19:44 +0530

    Update for dashboard

--- jessy-cabs/src/component/Dashboard/MainDash/MainDash.jsx
+++ jessy-cabs/src/component/Dashboard/MainDash/MainDash.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./MainDash.css";
-import RightSide from "../RightSide/RightSide";
-import Cards from "../Cards/Cards";
+import RightSide from "./RightSide/RightSide";
+import Cards from "./Cards/Cards";
 import Table from "./Table/Table";
 const MainDash = () => {
   return (

f63e63d5fa9169befbadd0d57699297d2c55d1a5
Abdul Fahad <fahadlee2000727@gmail.com>
2023-11-07 15:32:27 +0530

    Update for dashboard page

--- jessy-cabs/src/component/Billings/billingMain/PaymentDetail/PaymentDetail.jsx
+++ jessy-cabs/src/component/Billings/billingMain/PaymentDetail/PaymentDetail.jsx
@@ -192,7 +192,7 @@ const PaymentDetail = () => {
                 </div>
                 <div className="input" style={{ width: "230px" }}>
                   <div className="icone">
-                    <HailOutlinedIcon color="actions" />
+                    <HailOutlinedIcon color="action" />
                   </div>
                   <Autocomplete
                     fullWidth

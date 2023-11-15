10adf32f674a545df8eecb0944a43896ebbdcd8f
akash02899 <akash02899@gmail.com>
2023-11-15 18:22:20 +0530

    dashboard page updated

--- jessy-cabs/Backend/customer_master/Router/Billing/payment/payment.js
+++ jessy-cabs/Backend/customer_master/Router/Billing/payment/payment.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../../../db');
 
+const { subMonths, startOfMonth, endOfMonth, format } = require('date-fns');
+
+
 router.get('/organizationoptions', (req, res) => {
     db.query('SELECT DISTINCT customer FROM customers', (err, rows) => {
         if (err) {
@@ -13,56 +16,6 @@ router.get('/organizationoptions', (req, res) => {
         return res.status(200).json(organizations);
     });
 });
-
-// router.get('/payment-details', (req, res) => {
-//     const { organization, fromDate, toDate } = req.query;
-
-//     let query = 'SELECT * FROM billing';
-//     let params = [];
-
-//     if (organization) {
-//         query += ' AND customer = ?';
-//         params.push(organization);
-//     }
-
-//     if (fromDate && toDate) {
-//         query += ' AND Billingdate >= ? AND Billingdate <= DATE_ADD(?, INTERVAL 1 DAY)';
-//         params.push(fromDate);
-//         params.push(toDate);
-//     }
-
-//     db.query(query, params, (err, result) => {
-//         if (err) {
-//             console.error('Error retrieving booking details from MySQL:', err);
-//             return res.status(500).json({ error: 'Failed to retrieve booking details from MySQL' });
-//         }
-//         return res.status(200).json(result);
-//     });
-// });
-
-// router.get('/payment-details', (req, res) => {
-//     const { billingno, customer, fromDate, toDate } = req.query;
-//     console.log('Query parameters:', billingno, customer, fromDate, toDate);
-//     // const query = `SELECT * FROM billing WHERE billingno=? AND customer = ? AND Billingdate >= ? AND Billingdate <= ? AND Billingdate = ?`;
-//     const query = `
-//     SELECT *
-//     FROM billing
-//     WHERE billingno = ? 
-//     AND customer = ? 
-//     AND Billingdate >= ? 
-//     AND Billingdate <= ?;
-// `;
-//     db.query(query, [billingno, customer, fromDate, toDate], (err, results) => {
-//         if (err) {
-//             console.error('Database query error: ', err);
-//             res.status(500).json({ error: 'Error fetching data' });
-//             return;
-//         }
-//         console.log('collected billing', results);
-//         res.json(results);
-//     }
-//     );
-// });
 
 router.get('/payment-details', (req, res) => {
     const { billingno, customer, fromDate, toDate } = req.query;
@@ -91,7 +44,7 @@ router.get('/payment-details', (req, res) => {
     });
 });
 
-
+//totalamount of billing
 router.get('/totalAmount_from_billing', (req, res) => {
     const query = 'SELECT SUM(Totalamount) AS total FROM billing';
     // console.log('query from payment', query);
@@ -106,6 +59,119 @@ router.get('/totalAmount_from_billing', (req, res) => {
             res.json({ totalAmount });
         }
     });
+});
+
+const fetchDataFromDatabase = async (startDate, endDate) => {
+    const query = `
+        SELECT 
+            SUM(Totalamount) AS totalAmount,
+            SUM(paidamount) AS totalPaid,
+            SUM(pendingamount) AS totalPending
+        FROM billing
+        WHERE Billingdate >= ? AND Billingdate <= ?;
+    `;
+
+    return new Promise((resolve, reject) => {
+        db.query(query, [startDate, endDate], (err, result) => {
+            if (err) {
+                console.error('Error executing query:', err);
+                reject(err);
+            } else {
+                const { totalAmount, totalPaid, totalPending } = result[0];
+                console.log(`Data for ${startDate} to ${endDate}:`, { totalAmount, totalPaid, totalPending });
+                resolve({ totalAmount, totalPaid, totalPending });
+            }
+        });
+    });
+};
+
+const calculatePercentageChange = (currentMonthData, lastMonthData) => {
+    const calculatePercentage = (current, last) => {
+        if (last === 0) {
+            return current !== 0 ? 100 : 0;
+        }
+        return ((current - last) / Math.abs(last)) * 100;
+    };
+
+    return {
+        totalAmount: calculatePercentage(currentMonthData.totalAmount, lastMonthData.totalAmount),
+        totalPaid: calculatePercentage(currentMonthData.totalPaid, lastMonthData.totalPaid),
+        totalPending: calculatePercentage(currentMonthData.totalPending, lastMonthData.totalPending),
+    };
+};
+
+
+
+const fetchDataForDateRange = async (startDate, endDate) => {
+    const query = `
+        SELECT 
+            SUM(Totalamount) AS totalAmount,
+            SUM(paidamount) AS totalPaid,
+            SUM(pendingamount) AS totalPending
+        FROM billing
+        WHERE Billingdate >= ? AND Billingdate <= ?;
+    `;
+
+    return new Promise((resolve, reject) => {
+        db.query(query, [startDate, endDate], (err, result) => {
+            if (err) {
+                console.error('Error executing query:', err);
+                reject(err);
+            } else {
+                const { totalAmount, totalPaid, totalPending } = result[0];
+                console.log(`Data for ${startDate} to ${endDate}:`, { totalAmount, totalPaid, totalPending });
+                resolve({ totalAmount, totalPaid, totalPending });
+            }
+        });
+    });
+};
+
+router.get('/total_amounts_from_billing', async (req, res) => {
+    const currentDate = new Date();
+    const currentMonthStartDate = startOfMonth(new Date());
+    const currentMonthEndDate = endOfMonth(new Date());
+
+    const lastMonthStartDate = startOfMonth(subMonths(currentDate, 1));
+    const lastMonthEndDate = endOfMonth(subMonths(currentDate, 1));
+
+    const formatDateString = (date) => format(date, 'yyyy-MM-dd');
+
+    // const currentDateString = formatDateString(currentDate);
+
+    const currentMonthStartDateString = formatDateString(currentMonthStartDate);
+    const currentMonthEndDateString = formatDateString(currentMonthEndDate);
+
+
+    const lastMonthStartDateString = formatDateString(lastMonthStartDate);
+    const lastMonthEndDateString = formatDateString(lastMonthEndDate);
+
+    // console.log('Current Date:', currentDateString);
+
+    console.log('Last Month Start Date:', currentMonthStartDateString);
+    console.log('Last Month End Date:', currentMonthEndDateString);
+
+    console.log('Last Month Start Date:', lastMonthStartDateString);
+    console.log('Last Month End Date:', lastMonthEndDateString);
+
+    try {
+        // const currentData = await fetchDataFromDatabase(currentDateString);
+
+        const currentMonthData = await fetchDataFromDatabase(currentMonthStartDateString, currentMonthEndDateString);
+
+
+        const lastMonthData = await fetchDataForDateRange(lastMonthStartDateString, lastMonthEndDateString);
+
+        const result = {
+            current: currentMonthData,
+            lastMonth: lastMonthData,
+            percentageChange: calculatePercentageChange(currentMonthData, lastMonthData),
+        };
+
+        res.json(result);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 module.exports = router;
--- jessy-cabs/src/component/Dashboard/MainDash/Cards/Card/Card.jsx
+++ jessy-cabs/src/component/Dashboard/MainDash/Cards/Card/Card.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from "react";
 import "./Card.css";
 import { CircularProgressbar } from "react-circular-progressbar";

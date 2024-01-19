cbeb3488769b84a5d5b609f2bf5b09073ceb23a6
akash02899 <akash02899@gmail.com>
2024-01-19 10:34:25 +0530

    permission page updated

--- jessy-cabs/Backend/customer_master/Router/driver/driver.js
+++ jessy-cabs/Backend/customer_master/Router/driver/driver.js
@@ -51,4 +51,60 @@ router.get('/drivermaster', (req, res) => {
 });
 // End driver master database
 
+//for user permission
+router.post('/save-permissions', async (req, res) => {
+  const { userId, permissions } = req.body;
+
+  try {
+    // Clear existing permissions for the user
+    await db.query('DELETE FROM user_permissions WHERE user_id = ?', [userId]);
+
+    // Insert new permissions
+    for (const permission of permissions) {
+      await db.query(
+        'INSERT INTO user_permissions (user_id, page_name, read_permission, new_permission, modify_permission, delete_permission) VALUES (?, ?, ?, ?, ?, ?)',
+        [userId, permission.name, permission.read, permission.new, permission.modify, permission.delete]
+      );
+    }
+
+    res.status(200).json({ message: 'Permissions saved successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+//for collect userid
+router.get('/userdata/:username', (req, res) => {
+  const username = req.params.username;
+
+  db.query('SELECT * FROM usercreation WHERE username = ?', username, (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to retrieve route data from MySQL' });
+    }
+
+    if (result.length === 0) {
+      return res.status(404).json({ error: 'Route data not found' });
+    }
+
+    const routeData = result;
+    return res.status(200).json(routeData);
+  });
+});
+
+// collect data from billing database
+router.get('/userdataid/:user_id', (req, res) => {
+  const user_id = req.params.user_id;
+  db.query('SELECT * FROM user_permissions WHERE user_id = ?', user_id, (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to retrieve user permissions from MySQL' });
+    }
+    if (result.length === 0) {
+      return res.status(404).json({ error: 'User permissions not found' });
+    }
+    const userdetails = result;
+    return res.status(200).json(userdetails);
+  });
+});
+
 module.exports = router;

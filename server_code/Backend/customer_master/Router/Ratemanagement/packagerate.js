6fff5f027eea561970c181da628e63621da1ba47
Ayyanar022 <ayan.prof022@gmail.com>
2024-04-15 10:11:58 +0530

    pakage rate full page work

--- server_code/Backend/customer_master/Router/Ratemanagement/packagerate.js
+++ server_code/Backend/customer_master/Router/Ratemanagement/packagerate.js
@@ -2,17 +2,44 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../../db');
 
-// Add Rate Management database
-router.post('/ratemanagement', (req, res) => {
-    const bookData = req.body;
-    db.query('INSERT INTO ratemanagement SET ?', bookData, (err, result) => {
-        if (err) {
-            return res.status(500).json({ error: "Failed to insert data into MySQL" });
-        }
-        return res.status(200).json({ message: "Data inserted successfully" });
+
+
+// // Add Rate Management database--------------------------------------------------------
+
+router.post('/ratemanagement-add', (req, res) => {
+    const bookDataArray = req.body; // Assuming req.body is an array of objects
+
+    // Check if req.body is an array
+    if (!Array.isArray(bookDataArray)) {
+        return res.status(400).json({ error: "Request body must be an array" });
+    }
+
+    // Insert each object in the array as a separate row in the database
+    const insertQueries = bookDataArray.map(bookData => {
+        return new Promise((resolve, reject) => {
+            db.query('INSERT INTO ratemanagement SET ?', bookData, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
     });
+
+    // Execute all insert queries concurrently
+    Promise.all(insertQueries)
+        .then(() => {
+            return res.status(200).json({ message: "Data inserted successfully" });
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: "Failed to insert data into MySQL" });
+        });
 });
-// delete Rate Management data
+
+
+// delete Rate Management data-------------------------------------------------------------
 router.delete('/ratemanagement/:id', (req, res) => {
     const customerid = req.params.id;
     db.query('DELETE FROM ratemanagement WHERE id = ?', customerid, (err, result) => {
@@ -26,9 +53,10 @@ router.delete('/ratemanagement/:id', (req, res) => {
     });
 });
 // update Rate Management details
-router.put('/ratemanagement/:id', (req, res) => {
+router.put('/ratemanagement-edit/:id', (req, res) => {
     const customerId = req.params.id;
     const updatedCustomerData = req.body;
+
     db.query('UPDATE ratemanagement SET ? WHERE id = ?', [updatedCustomerData, customerId], (err, result) => {
         if (err) {
             return res.status(500).json({ error: "Failed to update data in MySQL" });

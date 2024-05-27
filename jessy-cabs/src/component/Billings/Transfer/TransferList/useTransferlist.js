06eb368216baa4deacff16577ef5b5a2113f15a5
Ayyanar022 <ayan.prof022@gmail.com>
2024-05-27 16:13:06 +0530

    404 billing

--- jessy-cabs/src/component/Billings/Transfer/TransferList/useTransferlist.js
+++ jessy-cabs/src/component/Billings/Transfer/TransferList/useTransferlist.js
@@ -5,12 +5,10 @@ import dayjs from "dayjs";
 import { saveAs } from "file-saver";
 import { Organization } from "../../billingMain/PaymentDetail/PaymentDetailData";
 import { APIURL } from "../../../url";
-import { useData } from "../../../Dashboard/Maindashboard/DataContext";
 import Excel from 'exceljs';
 
 const useTransferlist = () => {
   const apiUrl = APIURL;
-  // const user_id = localStorage.getItem("useridno");
   const [selectedStatus, setSelectedStatus] = useState("");
   const [rows, setRows] = useState([]);
   const [error, setError] = useState(false);
@@ -24,213 +22,184 @@ const useTransferlist = () => {
   const [warning, setWarning] = useState(false);
   const [warningMessage] = useState({});
   const [servicestation, setServiceStation] = useState("");
-  const { setOrganizationName } = useData()
 
 
 
-  useEffect(() => {
-    const organizationNames = async () => {
-      try {
-        const response = await axios.get(`${apiUrl}/customers`);
-        const organisationData = response?.data;
-        const names = organisationData.map(res => res.customer);
-        setOrganizationName(names);
-      } catch (error) {
-        console.error('Error fetching organization names:', error);
-      }
-    };
-    organizationNames();
-  }, [apiUrl, setOrganizationName])
 
-
-  // const convertToCSV = (data) => {
-  //   const header = columns.map((column) => column.headerName).join(",");
-  //   const rows = data.map((row) =>
-  //     columns.map((column) => row[column.field]).join(",")
-  //   );
-  //   return [header, ...rows].join("\n");
-  // };
-  // const handleExcelDownload = () => {
-  //   const csvData = convertToCSV(rows);
-  //   const blob = new Blob([csvData], { type: "text/csv;charset=utf-8" });
-  //   saveAs(blob, "Transfer_list.csv");
-
-
-  // };
 
   const handleExcelDownload = async () => {
     const workbook = new Excel.Workbook();
     const workSheetName = 'Worksheet-1';
-    console.log(rows,"exceldata")
+    console.log(rows, "exceldata")
 
     try {
 
-        const fileName = "Transfer_list"
-        // creating one worksheet in workbook
-        const worksheet = workbook.addWorksheet(workSheetName);
-        const headers = Object.keys(rows[0]);
-        const idIndex = headers.indexOf('id');
-            if (idIndex !== -1) {
-                headers.splice(idIndex, 1);
-                headers.unshift('id');
-               
-            }
-         
-//         console.log(headers,"hed")
-        const columns = headers.map(key => ({ key, header: key }));
-//         worksheet.columns = columnsexcel
-      
-        worksheet.columns = columns;
+      const fileName = "Transfer_list"
+      // creating one worksheet in workbook
+      const worksheet = workbook.addWorksheet(workSheetName);
+      const headers = Object.keys(rows[0]);
+      const idIndex = headers.indexOf('id');
+      if (idIndex !== -1) {
+        headers.splice(idIndex, 1);
+        headers.unshift('id');
+
+      }
+
+      //         console.log(headers,"hed")
+      const columns = headers.map(key => ({ key, header: key }));
+      //         worksheet.columns = columnsexcel
+
+      worksheet.columns = columns;
 
 
-        // updated the font for first row.
-        worksheet.getRow(1).font = { bold: true };
+      // updated the font for first row.
+      worksheet.getRow(1).font = { bold: true };
 
-        // Set background color for header cells
-        worksheet.getRow(1).eachCell((cell, colNumber) => {
-            cell.fill = {
-                type: 'pattern',
-                pattern: 'solid',
-                fgColor: { argb: '9BB0C1' } // Green background color
-            };
-        });
+      // Set background color for header cells
+      worksheet.getRow(1).eachCell((cell, colNumber) => {
+        cell.fill = {
+          type: 'pattern',
+          pattern: 'solid',
+          fgColor: { argb: '9BB0C1' } // Green background color
+        };
+      });
 
 
-        worksheet.getRow(1).height = 30;
-        // loop through all of the columns and set the alignment with width.
+      worksheet.getRow(1).height = 30;
+      // loop through all of the columns and set the alignment with width.
+      worksheet.columns.forEach((column) => {
+        column.width = column.header.length + 5;
+        column.alignment = { horizontal: 'center', vertical: 'middle' };
+      });
+
+      rows.forEach((singleData, index) => {
+
+
+        worksheet.addRow(singleData);
+
+        // Adjust column width based on the length of the cell values in the added row
         worksheet.columns.forEach((column) => {
-            column.width = column.header.length + 5;
-            column.alignment = { horizontal: 'center', vertical: 'middle' };
+          const cellValue = singleData[column.key] || ''; // Get cell value from singleData or use empty string if undefined
+          const cellLength = cellValue.toString().length; // Get length of cell value as a string
+          const currentColumnWidth = column.width || 0; // Get current column width or use 0 if undefined
+
+          // Set column width to the maximum of current width and cell length plus extra space
+          column.width = Math.max(currentColumnWidth, cellLength + 5);
         });
+      });
 
-        rows.forEach((singleData, index) => {
+      // loop through all of the rows and set the outline style.
+      worksheet.eachRow({ includeEmpty: false }, (row) => {
+        // store each cell to currentCell
+        const currentCell = row._cells;
 
+        // loop through currentCell to apply border only for the non-empty cell of excel
+        currentCell.forEach((singleCell) => {
 
-            worksheet.addRow(singleData);
+          const cellAddress = singleCell._address;
 
-            // Adjust column width based on the length of the cell values in the added row
-            worksheet.columns.forEach((column) => {
-                const cellValue = singleData[column.key] || ''; // Get cell value from singleData or use empty string if undefined
-                const cellLength = cellValue.toString().length; // Get length of cell value as a string
-                const currentColumnWidth = column.width || 0; // Get current column width or use 0 if undefined
-
-                // Set column width to the maximum of current width and cell length plus extra space
-                column.width = Math.max(currentColumnWidth, cellLength + 5);
-            });
+          // apply border
+          worksheet.getCell(cellAddress).border = {
+            top: { style: 'thin' },
+            left: { style: 'thin' },
+            bottom: { style: 'thin' },
+            right: { style: 'thin' },
+          };
         });
+      });
+      // write the content using writeBuffer
+      const buf = await workbook.xlsx.writeBuffer();
 
-        // loop through all of the rows and set the outline style.
-        worksheet.eachRow({ includeEmpty: false }, (row) => {
-            // store each cell to currentCell
-            const currentCell = row._cells;
-
-            // loop through currentCell to apply border only for the non-empty cell of excel
-            currentCell.forEach((singleCell) => {
-
-                const cellAddress = singleCell._address;
-
-                // apply border
-                worksheet.getCell(cellAddress).border = {
-                    top: { style: 'thin' },
-                    left: { style: 'thin' },
-                    bottom: { style: 'thin' },
-                    right: { style: 'thin' },
-                };
-            });
-        });
-        // write the content using writeBuffer
-        const buf = await workbook.xlsx.writeBuffer();
-
-        // download the processed file
-        saveAs(new Blob([buf]), `${fileName}.xlsx`);
+      // download the processed file
+      saveAs(new Blob([buf]), `${fileName}.xlsx`);
     } catch (error) {
-        console.error('<<<ERRROR>>>', error);
-        console.error('Something Went Wrong', error.message);
+      console.error('<<<ERRROR>>>', error);
+      console.error('Something Went Wrong', error.message);
     } finally {
-        // removing worksheet's instance to create new one
-        workbook.removeWorksheet(workSheetName);
+      // removing worksheet's instance to create new one
+      workbook.removeWorksheet(workSheetName);
     }
 
-}
+  }
 
 
-const handlePdfDownload = () => {
+  const handlePdfDownload = () => {
     const pdf = new jsPDF({
-        orientation: "landscape",
-        unit: "mm",
-        format: "tabloid" // [width, height] in inches
+      orientation: "landscape",
+      unit: "mm",
+      format: "tabloid" // [width, height] in inches
     });
     pdf.setFontSize(10);
     pdf.setFont('helvetica', 'normal');
     pdf.text("Transfer_list", 10, 10);
-     const header = Object.keys(rows[0]);
-     const idIndex = header.indexOf('id');
-            if (idIndex !== -1) {
-                header.splice(idIndex, 1);
-                header.unshift('id');
-               
-            }
+    const header = Object.keys(rows[0]);
+    const idIndex = header.indexOf('id');
+    if (idIndex !== -1) {
+      header.splice(idIndex, 1);
+      header.unshift('id');
+
+    }
 
     // Extracting body
     const body = rows.map(row => Object.values(row));
 
     let fontdata = 1;
     if (header.length <= 13) {
-        fontdata = 16;
+      fontdata = 16;
     }
     else if (header.length >= 14 && header.length <= 20) {
-        fontdata = 11;
+      fontdata = 11;
     } else if (header.length >= 21 && header.length <= 23) {
-        fontdata = 9;
+      fontdata = 9;
     }
     else if (header.length >= 24 && header.length <= 26) {
-        fontdata = 7;
+      fontdata = 7;
     }
     else if (header.length >= 27 && header.length <= 30) {
-        fontdata = 6;
+      fontdata = 6;
     }
     else if (header.length >= 31 && header.length <= 35) {
-        fontdata = 4;
+      fontdata = 4;
     }
     else if (header.length >= 36 && header.length <= 40) {
-        fontdata = 4;
+      fontdata = 4;
     }
     else if (header.length >= 41 && header.length <= 46) {
-        fontdata = 2;
+      fontdata = 2;
     }
-    
+
     pdf.autoTable({
-        head: [header],
-        body: body,
-        startY: 20,
+      head: [header],
+      body: body,
+      startY: 20,
 
-        headStyles: {
-            // fontSize: 5,
-            fontSize: fontdata,
-            cellPadding: 1.5, // Decrease padding in header
+      headStyles: {
+        // fontSize: 5,
+        fontSize: fontdata,
+        cellPadding: 1.5, // Decrease padding in header
 
-            minCellHeigh: 8,
-            valign: 'middle',
+        minCellHeigh: 8,
+        valign: 'middle',
 
-            font: 'helvetica', // Set font type for body
+        font: 'helvetica', // Set font type for body
 
-            cellWidth: 'wrap',
-            // cellWidth: 'auto'
-        },
+        cellWidth: 'wrap',
+        // cellWidth: 'auto'
+      },
 
-        bodyStyles: {
-            // fontSize:4,
-            // fontSize: fontdata-1
-            fontSize: fontdata-1,
-            valign: 'middle',
-            //  cellWidth: 'wrap',
-            cellWidth: 'auto'
-            // Adjust the font size for the body
+      bodyStyles: {
+        // fontSize:4,
+        // fontSize: fontdata-1
+        fontSize: fontdata - 1,
+        valign: 'middle',
+        //  cellWidth: 'wrap',
+        cellWidth: 'auto'
+        // Adjust the font size for the body
 
-        },
-        columnWidth: 'auto'
+      },
+      columnWidth: 'auto'
 
-});
+    });
     const scaleFactor = pdf.internal.pageSize.getWidth() / pdf.internal.scaleFactor * 1.5;
     console.log(scaleFactor, "SCALE")
 
@@ -238,7 +207,7 @@ const handlePdfDownload = () => {
     pdf.scale(scaleFactor, scaleFactor);
     const pdfBlob = pdf.output('blob');
     saveAs(pdfBlob, 'Transfer_list.pdf');
-};
+  };
   // const handlePdfDownload = () => {
   //   const pdf = new jsPDF();
   //   pdf.setFontSize(12);

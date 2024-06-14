6d1ce6bdf04b4ffdf6804c51b81468d7691430e4
Abdul Fahad <fahadlee2000727@gmail.com>
2024-06-14 14:07:35 +0530

    Update

--- jessy-cabs/src/component/url.js
+++ jessy-cabs/src/component/url.js
@@ -1,4 +1,4 @@
 // export const APIURL = 'http://192.168.0.101:8081';
-// export const APIURL = 'http://3.84.108.173:8081';
-export const APIURL = 'http://localhost:8081';
+export const APIURL = 'http://3.84.108.173:8081';
+// export const APIURL = 'http://localhost:8081';
 // export const APIURL = 'http://54.234.161.76:8081';

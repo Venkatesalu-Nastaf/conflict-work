7063da1d52e76aac85fb48fde6a0a7862dd5b741
AjayRaman0806 <111749069+AjayRaman0806@users.noreply.github.com>
2024-06-19 11:17:10 +0530

    update  url

--- jessy-cabs/src/setupProxy.js
+++ jessy-cabs/src/setupProxy.js
@@ -1,10 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
+import { APIURL } from './component/url';
 
 module.exports = function (app) {
   app.use(
     '/signature_photos',
     createProxyMiddleware({
-      target: 'http://localhost:8081', // Your Express server's URL
+      // target: 'http://localhost:8081', // Your Express server's URL
+      target: `${APIURL}`, // Your Express server's URL
       changeOrigin: true,
     })
   );

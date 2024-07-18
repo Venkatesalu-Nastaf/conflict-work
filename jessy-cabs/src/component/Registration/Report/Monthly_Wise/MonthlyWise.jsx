d3ee347ad58fad1d90171f8eb9eb8a901028dfd9
Abdul Fahad <fahadlee2000727@gmail.com>
2024-07-18 10:27:33 +0530

    Update

---
+++ jessy-cabs/src/component/Registration/Report/Monthly_Wise/MonthlyWise.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MonthlyWise = () => {
+  return (
+    <div>MonthlyWise</div>
+  )
+}
+
+export default MonthlyWise
---
+++ jessy-cabs/src/component/Registration/Report/Selected_Sales/SelectedSales.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SelectedSales = () => {
+  return (
+    <div>SelectedSales</div>
+  )
+}
+
+export default SelectedSales

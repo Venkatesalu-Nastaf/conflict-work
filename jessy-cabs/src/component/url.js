0dcc5350c8ab6195854833541422d5ce3ef44ce3
Abdul Fahad <fahadlee2000727@gmail.com>
2024-08-09 11:16:26 +0530

    Update for build code

--- jessy-cabs/src/component/url.js
+++ jessy-cabs/src/component/url.js
@@ -2,6 +2,6 @@ module.exports = {
     // APIURL: "http://localhost:8081",
     APIURL: "http://3.84.108.173:8081", 
     // this apiurl for driverapp
-    Apiurltransfer: "http://54.234.174.93:7000"
+    Apiurltransfer: "http://75.101.215.49:7000"
 }
 

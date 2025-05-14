48c92c3d9fde405b5d9e9d610bcc4f035de7c60a
Venkatesalu-Nastaf <venkatesalu@nastaf.com>
2025-05-14 18:46:25 +0530

    api key work is done

--- jessy-cabs/src/component/ApiKey/mapApiKey.js
+++ jessy-cabs/src/component/ApiKey/mapApiKey.js
@@ -1,4 +1,3 @@
-
-
 // export const ApiKey = "AIzaSyCn47dR5-NLfhq0EqxlgaFw8IEaZO5LnRE";
-export const ApiKey = "AIzaSyBReWXII6pzCf226Qybl5heJhIyV31uxsk";
+// export const ApiKey = "AIzaSyBReWXII6pzCf226Qybl5heJhIyV31uxsk";
+export const ApiKey =    "AIzaSyBP2X3r7f472PLSfZPxKokADqxAYSWm4Sc";

12365194d4e48c9f4c2cfe99050c5752bf9ba3a9
Shuruthi-Manoharan <shuruthi@nastaf.com>
2025-05-14 19:03:51 +0530

    update

---
+++ jessy-cabs/src/component/Dashboard/MainDash/Charts/ProfitData.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer
+} from "recharts";
+import "./ProfitData.css"
+
+const ProfitData = ({ data, xKey, title }) => {
+  return (
+    <div className='barchart-data'>
+      <h2 style={{ textAlign: 'center',color:"black" }}>{title || "Profit Summary"}</h2>
+      <ResponsiveContainer width="100%" height={200}>
+        <BarChart data={data}>
+          <XAxis dataKey={xKey}  />
+          <YAxis  />
+          <Tooltip
+            contentStyle={{ backgroundColor: 'black', borderColor: 'green', color: 'white' }}
+            labelStyle={{ color: 'white' }}
+            itemStyle={{ color: 'white' }}
+          />
+          <Legend wrapperStyle={{ color: 'white' }} />
+          <Bar dataKey="profit" fill="teal" radius={[4, 4, 0, 0]} />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default ProfitData;
